ab8e6ad776de3b5a1c1657c828bd3b658360445a
NellyNell <129366078+TekNellCo@users.noreply.github.com>
2024-11-02 18:26:53 -0400

    toggles work for buttons and also dynamically adds images to each div

--- script.js
+++ script.js
@@ -5,22 +5,69 @@ const body = document.querySelector('.body');
 const locationPopUp = document.querySelector('.locationPopUp');
 const searchBar = document.querySelector('.searchBar');
 const themePopUp = document.querySelector('.themePopUp');
-const themes = document.querySelector('.themes');
+const themes = document.querySelectorAll('.themes');
 const languagePopUp = document.querySelector('.languagePopUp');
 const languages = document.querySelector('.languages');
+const image = document.querySelector('.image');
 
-///////Maipulates popups by changing classes in css
+// const test = document.querySelector('.test');
+// test.style = 'color: red';
+
+///////adds css classes to popups while resetting other buttons
 city.addEventListener('click', () => {
-  locationPopUp.classList.add('locationPopUpAfter');
-  searchBar.classList.add('searchBarAfter');
+  locationPopUp.classList.toggle('locationPopUpAfter');
+  searchBar.classList.toggle('searchBarAfter');
+  themes.className = 'themes';
+  themePopUp.className = 'themePopUp';
+  languages.className = 'languages';
+  languagePopUp.className = 'languagePopUp';
+  themes.forEach((theme) => {
+    theme.classList.remove('themesAfter');
+    theme.classList.remove('one');
+  });
 });
-
 theme.addEventListener('click', () => {
-  themePopUp.classList.add('themePopUpAfter');
-  themes.classList.add('themesAfter');
+  let imageNum = 0;
+
+  themes.forEach((theme) => {
+    if (theme.children.length > 0) {
+      theme.innerHTML = '';
+    } else {
+    }
+    imageNum++;
+    theme.classList.toggle('themesAfter');
+    theme.classList.toggle('one');
+    let img = document.createElement('div');
+    theme.append(img);
+    img.className = 'image';
+    img.style.backgroundImage = `url(./images/${imageNum}.png)`;
+  });
+  themePopUp.classList.toggle('themePopUpAfter');
+  searchBar.className = 'searchBar';
+  locationPopUp.className = 'locationPopUp';
+  languages.className = 'languages';
+  languagePopUp.className = 'languagePopUp';
 });
 
 lang.addEventListener('click', () => {
-  languagePopUp.classList.add('languagePopUpAfter');
-  languages.classList.add('languagesAfter');
+  languagePopUp.classList.toggle('languagePopUpAfter');
+  languages.classList.toggle('languagesAfter');
+  searchBar.className = 'searchBar';
+  locationPopUp.className = 'locationPopUp';
+  themes.className = 'themes';
+  themePopUp.className = 'themePopUp';
+  themes.forEach((theme) => {
+    theme.classList.remove('themesAfter');
+    theme.classList.remove('one');
+  });
 });
+
+//   searchBar.className = 'searchBar';
+//   locationPopUp.className = 'locationPopUp';
+//   themes.className = 'themes';
+//   themePopUp.className = 'themePopUp';
+//   languages.className = 'languages';
+//   languagePopUp.className = 'languagePopUp';
+// themes.forEach((theme) => {
+//   for (let i = 0; i < 3; i++) console.log(theme.i);
+// });
